b8d59a4ef4fe341ef06a154811ad9c5eea16a601
Tuukka-94Pu <tuukka.toivola@vuoksi.fi>
2026-01-22 14:12:14 +0200

    added playerStats

---
+++ Scripts/playerStats.cs
@@ -0,0 +1,34 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class playerStats : MonoBehaviour
+{
+    public int health;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        health = 100;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(health <= 0)
+        {
+            OnDeath();
+        }
+
+    }
+    public void takeDamage(int damage)
+    {
+        health -= damage;
+    }
+
+    public void OnDeath()
+    {
+        //Do death things
+    }
+}
