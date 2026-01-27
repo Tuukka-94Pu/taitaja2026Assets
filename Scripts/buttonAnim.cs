20177cd9e3f3e3f2f9dd64fe89bb52eaab0c35e8
Tuukka-94Pu <tuukka.toivola@vuoksi.fi>
2026-01-27 13:17:09 +0200

    animated button

---
+++ Scripts/buttonAnim.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class buttonAnim : MonoBehaviour, IPointerEnterHandler , IPointerExitHandler
+{
+    //Add to a Button to animate it being highlighted
+
+
+    float scaleFun = 1.0f;
+
+    void Update()
+    {
+        this.transform.localScale = new Vector3(scaleFun, scaleFun);
+    }
+    public void OnPointerEnter(PointerEventData idk)
+    {
+        Debug.Log("Pointer entered");
+        StartCoroutine(UpscaleWait());
+    }
+
+    public void OnPointerExit(PointerEventData idk)
+    {
+        Debug.Log("Ponter leaved");
+        StartCoroutine(DownscaleWait());
+    }
+    private IEnumerator DownscaleWait()
+    {
+        yield return new WaitForSeconds(0.05f);
+        scaleFun -= 0.1f;
+        yield return new WaitForSeconds(0.05f);
+        scaleFun -= 0.1f;
+    }
+    private IEnumerator UpscaleWait()
+    {
+        yield return new WaitForSeconds(0.05f);
+        scaleFun += 0.1f;
+        yield return new WaitForSeconds(0.05f);
+        scaleFun += 0.1f;
+    }
+}
