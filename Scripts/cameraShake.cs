4f0ee292fb1c32cabc3049535cd30303115d14d0
Tuukka-94Pu <tuukka.toivola@vuoksi.fi>
2026-01-27 10:01:53 +0200

    updated cameraShake

--- Scripts/cameraShake.cs
+++ Scripts/cameraShake.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class cameraShake : MonoBehaviour
 {
+    public GameObject player;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,12 @@ public class cameraShake : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        if(player.transform.hasChanged)
+        {
+            transform.localPosition = Random.insideUnitSphere * 0.02f;
+            player.transform.hasChanged = false;
+        }       
+       
     }
 }
