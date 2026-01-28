5082826cdb90a3a80fbd1f0988c2be735370ae24
Tuukka-94Pu <tuukka.toivola@vuoksi.fi>
2026-01-28 10:50:33 +0200

    death mechaniscs

--- Scripts/playerStats.cs
+++ Scripts/playerStats.cs
@@ -5,13 +5,6 @@ public class playerStats : MonoBehaviour
 {
     public int health;
 
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        health = 100;        
-    }
-
     // Update is called once per frame
     void Update()
     {
@@ -29,7 +22,9 @@ public class playerStats : MonoBehaviour
 
     public void OnDeath()
     {
-        //Do death things
+        print("You die");
+        var death = GameObject.Find("GameManager").GetComponent<GameManager>();
+        death.OnDeath();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
